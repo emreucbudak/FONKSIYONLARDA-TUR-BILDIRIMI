95af5f8c5989465e8831560d5dd8df7fa5d0939d
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-25 22:57:06 +0300

    Add files via upload

---
+++ main (25).php
@@ -0,0 +1,20 @@
+<?php
+/******************************************************************************
+
+Emre Üçbudak Karadeniz Teknik Üniversitesi
+
+*******************************************************************************/
+// PHP FONKSIYONLARA GIRIS
+// Fonksıyonlarda varlık kontrolu
+
+function islem(int $sayi1,int $sayi2) {
+    echo($sayi1+$sayi2);
+    
+}
+islem(15,25);
+
+
+
+
+
+?>
